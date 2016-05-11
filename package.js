12357c47354a5a564be54a67f545c74ed1e8bd6d
Muhamed Krlić <krlicmuhamed@gmail.com>
2016-05-11 19:37:55 +0200

    Specified lodash version constraint.

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
   api.use('ecmascript');
   api.use('jquery', 'client');
-  api.use('stevezhu:lodash', 'client');
+  api.use('stevezhu:lodash@4.11.1', 'client');
   api.export("meni");
   api.addFiles('lib/meni.js/meni.js', 'client');
 });

0423fefe506f493ed9cd4a5ad30a5a163769e333
Muhamed Krlić <krlicmuhamed@gmail.com>
2016-05-11 16:08:06 +0200

    Initial commit

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A convenient client javascript library for making dynamic menus',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/l0oky/meni.js-meteor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'menu.js/README.md'

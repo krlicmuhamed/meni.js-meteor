e637fa156b63383747d60673c3096415a5ea3fa1
Muhamed Krlić <krlicmuhamed@gmail.com>
2016-05-11 19:34:59 +0200

    Fixed a typo.

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
   git: 'https://github.com/l0oky/meni.js-meteor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'lib/menu.js/README.md'
+  documentation: 'lib/meni.js/README.md'
 });
 
 Package.onUse(function(api) {

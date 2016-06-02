20f4d0cbf3de221e64ef5e2b051c1239d8eeecb9
Muhamed Krlić <krlicmuhamed@gmail.com>
2016-06-02 23:59:24 +0200

    Version updatee.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'l0oky:meni.js',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'A convenient client javascript library for making dynamic tabbed menus',
   // URL to the Git repository containing the source code for this package.
